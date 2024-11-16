d5d7738accc5b0f90b2aaf911abd14e086e6592f
Michael Karbowiak <michael@karbowiak.dk>
2024-11-16 15:49:02 +0100

    Remove RedisQ from the Nitro Plugins, and move it to a standalone script instead

---
+++ scripts/RedisQ.ts
@@ -0,0 +1,22 @@
+
+const queueUrl = 'https://redisq.zkillboard.com/listen.php?queueID=evekill_js';
+const queueName = 'killmails';
+
+async function startListener() {
+    console.log('Starting redisQ listener');
+    while(true) {
+        const response = await fetch(queueUrl, {
+            headers: { 'User-Agent': 'EVE-KILL' }
+        });
+        const data = await response.json();
+
+        if (data?.package) {
+            const killmailId = data.package.killID;
+            const killmailHash = data.package.zkb.hash;
+
+            console.log(`New killmail: ${killmailId} - ${killmailHash}`);
+        }
+    }
+}
+
+startListener();
