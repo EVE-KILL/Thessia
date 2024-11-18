1eb72da02ec07bbb3785b9dd5d8d0569c6a5b632
Michael Karbowiak <michael@karbowiak.dk>
2024-11-18 21:10:57 +0100

    Change editorconfig indentation from 2 to 4 spaces, and rewrite the index.post.ts - remove the ability to query multiple killmail ids using a post, and use it for posting a new killmail instead

--- server/api/killmail/index.post.ts
+++ server/api/killmail/index.post.ts
@@ -1,24 +1,46 @@
-import { Killmails } from "../../models/Killmails";
-import { Killmail } from "../../../types/IKillmail";
+import { fetchESIKillmail } from "~/server/helpers/ESIData";
+import { parseKillmail } from "~/server/helpers/KillmailParser";
+import { ESIKillmail } from "~/types/IESIKillmail";
+import { Killmail } from "~/types/IKillmail";
 
 export default defineEventHandler(async (event) => {
-  const killmailIds = await readBody(event);
+    const body = await readBody(event);
 
-  // Verify killmailIds is an array, and it contains only numbers, and is not empty
-  if (
-    !Array.isArray(killmailIds) ||
-    killmailIds.length === 0 ||
-    killmailIds.some((id) => typeof id !== "number")
-  ) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: "Invalid killmailIds",
-    });
-  }
+    const { killmail_id, killmail_hash } = body;
 
-  const killmails: Killmail[] | null = await Killmails.find(
-    { killmail_id: { $in: killmailIds } },
-    { _id: 0 },
-  );
-  return killmails;
+    if (typeof killmail_id !== "number") {
+        return { error: "killmail_id is missing or not a number" };
+    }
+
+    if (typeof killmail_hash !== "string") {
+        return { error: "killmail_hash is missing or not a string" };
+    }
+
+    // Check if killmail already exists
+    let existingKillmail: ESIKillmail | null = await KillmailsESI.findOne({ killmail_id: killmail_id, killmail_hash: killmail_hash });
+    if (existingKillmail) {
+        return { error: "Killmail already exists" };
+    }
+
+    // Fetch killmail from ESI
+    let esiKillmail: ESIKillmail | null = await fetchESIKillmail(killmail_id, killmail_hash);
+    if (!esiKillmail) {
+        return { error: "Error fetching killmail from ESI" };
+    }
+
+    // Process killmail and save to database
+    let killmail: Killmail = await parseKillmail(esiKillmail);
+    if (!killmail) {
+        return { error: "Error parsing killmail" };
+    }
+
+    // Save killmail to database
+    let km = new Killmails(killmail);
+    try {
+        await km.save();
+    } catch (error) {
+        Killmails.updateOne({ killmail_id: killmail.killmail_id }, killmail);
+    }
+
+    return { success: "Killmail saved", esi: esiKillmail, killmail: killmail };
 });
