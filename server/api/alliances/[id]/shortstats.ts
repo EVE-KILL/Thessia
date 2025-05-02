6694a6bf2e6bc97b097dc41b42675c608d82f36c
Michael Karbowiak <michael@karbowiak.dk>
2025-05-02 19:40:52 +0200

    Use 0 to get all time instead of limiting the shortstats to 90 days

--- server/api/alliances/[id]/shortstats.ts
+++ server/api/alliances/[id]/shortstats.ts
@@ -3,14 +3,18 @@ import { sendError } from "h3";
 import { calculateShortStats } from "~/server/helpers/Stats";
 
 export default defineEventHandler(async (event: H3Event) => {
-    const { id } = event.context.params as { id: string };
-    if (!id) {
+    const allianceId: number | null = event.context.params?.id
+        ? Number.parseInt(event.context.params.id)
+        : null;
+    const query = getQuery(event);
+    const days: number = query?.days ? Number.parseInt(query.days as string) : 0;
+    if (!allianceId) {
         return sendError(event, createError({ statusCode: 400, statusMessage: "Missing alliance id" }));
     }
 
     try {
         // Default to 90 days for short stats
-        const stats = await calculateShortStats("alliance_id", Number(id), 90);
+        const stats = await calculateShortStats("alliance_id", allianceId, days);
         return stats;
     } catch (error) {
         return sendError(event, createError({ statusCode: 500, statusMessage: "Failed to fetch alliance short stats" }));
--- server/api/corporations/[id]/shortstats.ts
+++ server/api/corporations/[id]/shortstats.ts
@@ -3,14 +3,18 @@ import { sendError } from "h3";
 import { calculateShortStats } from "~/server/helpers/Stats";
 
 export default defineEventHandler(async (event: H3Event) => {
-    const { id } = event.context.params as { id: string };
-    if (!id) {
+    const corporationId: number | null = event.context.params?.id
+        ? Number.parseInt(event.context.params.id)
+        : null;
+    const query = getQuery(event);
+    const days: number = query?.days ? Number.parseInt(query.days as string) : 0;
+    if (!corporationId) {
         return sendError(event, createError({ statusCode: 400, statusMessage: "Missing corporation id" }));
     }
 
     try {
         // Default to 90 days for short stats
-        const stats = await calculateShortStats("corporation_id", Number(id), 90);
+        const stats = await calculateShortStats("corporation_id", corporationId, days);
         return stats;
     } catch (error) {
         return sendError(event, createError({ statusCode: 500, statusMessage: "Failed to fetch corporation short stats" }));
