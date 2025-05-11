7bd973491abc0e6cf5c002b6a08ff478421c1f89
Michael Karbowiak <michael@karbowiak.dk>
2025-05-11 22:40:20 +0200

    Remove colormode from the theme settings

--- src/theme/modern/nuxt.config.ts
+++ src/theme/modern/nuxt.config.ts
@@ -1,24 +1,16 @@
 const theme = process.env.THEME || "modern";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [`~/src/theme/${theme}/assets/main.css`],
+    css: [`~/src/theme/${theme}/assets/main.css`],
 
-  components: [
-    {
-      path: "~/src/theme/modern/components",
-      pathPrefix: false,
-    },
-    {
-      path: "~/src/theme/modern/components/common",
-      pathPrefix: false,
-    },
-  ],
-
-  colorMode: {
-    classSuffix: "",
-    storage: "cookie",
-    storageKey: "theme",
-    preference: "system",
-    fallback: "dark",
-  },
+    components: [
+        {
+            path: "~/src/theme/modern/components",
+            pathPrefix: false,
+        },
+        {
+            path: "~/src/theme/modern/components/common",
+            pathPrefix: false,
+        },
+    ],
 });
