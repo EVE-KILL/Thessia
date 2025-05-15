46d74ad45a3f27cc3644d68c1643d1b9b419ec96
Michael Karbowiak <michael@karbowiak.dk>
2025-05-15 22:02:27 +0200

    Do not cache getting comments

--- server/api/comments/[identifier]/index.get.ts
+++ server/api/comments/[identifier]/index.get.ts
@@ -1,7 +1,7 @@
 import { cliLogger } from "~/server/helpers/Logger";
 import { Comments } from "~/server/models/Comments";
 
-export default defineCachedEventHandler(async (event) => {
+export default defineEventHandler(async (event) => {
     const identifier = event.context.params?.identifier;
 
     if (!identifier) {
@@ -33,21 +33,5 @@ export default defineCachedEventHandler(async (event) => {
             statusCode: 500,
             statusMessage: "Failed to fetch comments",
         });
-    }
-}, {
-    maxAge: 60, // Using a maxAge of 60 seconds for comments
-    staleMaxAge: -1,
-    swr: true,
-    base: "redis", // Assuming redis is the default cache base
-    shouldBypassCache: (event) => {
-        return process.env.NODE_ENV !== "production";
-    },
-    getKey: (event) => {
-        // Construct a unique key for this endpoint.
-        // This file uses a dynamic parameter `[identifier]`. You MUST include
-        // this parameter in the cache key.
-        const identifier = event.context.params?.identifier;
-        // No query parameters are used in this endpoint.
-        return `comments:${identifier}:index`;
     }
 });
